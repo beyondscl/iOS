b9e55077516918c36851fce2a9cc9740914cc8bf
xianming <xianming@xianmingtekiMac-mini.local>
2018-01-19 10:36:14 +0800

    Initial Commit

---
+++ qzh/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  qzh
+//
+//  Created by xianming on 2018/1/19.
+//  Copyright © 2018年 hzqzh. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ qzh/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  qzh
+//
+//  Created by xianming on 2018/1/19.
+//  Copyright © 2018年 hzqzh. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
